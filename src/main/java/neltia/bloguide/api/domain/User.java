9d6e4ac5d6f1956f659fd7bbb70df94d260bd95f
neltia <dsz08082@naver.com>
2024-03-17 22:02:41 +0900

    add: MariaDB - 사용자 조회 기능 적용
    - 사용자 목록 조회 기능 추가 (DTO 미적용)
    - 단일 사용자 조회 기능 추가 (DTO 미적용)
    - 사용자 데이터 반환을 위한 gsonUtils 코드 추가

---
+++ src/main/java/neltia/bloguide/api/domain/User.java
@@ -0,0 +1,49 @@
+package neltia.bloguide.api.domain;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "user_tbl")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idx;
+
+    @Column(name = "user_id", nullable = false, length = 100)
+    private String userId;
+
+    @Column(name = "user_pw", nullable = false, length = 500)
+    private String userPw;
+
+    @Column(name = "user_name", nullable = false, length = 100)
+    private String userName;
+
+    @Column(name = "user_email", nullable = false, length = 300)
+    private String userEmail;
+
+    @Column(name = "supervisor", nullable = false)
+    private int supervisor;
+
+    @Column(name = "is_use", nullable = false)
+    private int isUse;
+
+    @Column(name = "user_before_pw", nullable = false, length = 500)
+    private String userBeforePw;
+
+    @Column(name = "reg_time", nullable = true)
+    private LocalDateTime regTime;
+
+    @Column(name = "login_time", nullable = true)
+    private LocalDateTime loginTime;
+
+    @Column(name = "pw_change_time", nullable = true)
+    private LocalDateTime pwdChangeTime;
+}
---
+++ src/main/java/neltia/bloguide/api/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package neltia.bloguide.api.repository;
+
+import neltia.bloguide.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findUserByIdx(int idx);
+}
